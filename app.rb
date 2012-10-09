8bf8660e061d19e01341afc7dd50be91fa6b8a92
Romoku <team.romoku@gmail.com>
2012-10-09 04:20:58 -0400

    fixed concert parser

--- app.rb
+++ app.rb
@@ -1,22 +1,13 @@
-# The primary requirement of a Sinatra application is the sinatra gem.
-# If you haven't already, install the gem with 'gem install sinatra'
+require_relative 'nine30concertsparser'
 require 'sinatra'
-
 require 'nokogiri'
 require 'open-uri'
 
-# sinatra allows us to respond to route requests with code.  Here we are 
-# responding to requests for the root document - the naked domain.
 get '/' do
-  # the first two lines are lifted directly from our previous script
-  url = "http://www.930.com/concerts/#/930/"
+  url = "http://www.930.com/concerts/"
   data = Nokogiri::HTML(open(url))
-  
-  # this line has only be adjusted slightly with the inclusion of an ampersand
-  # before concerts.  This creates an instance variable that can be referenced
-  # in our display logic (view).
-  @concerts = data.css('.concert_listing')
-  
-  # this tells sinatra to render the Embedded Ruby template /views/shows.erb
+  concertsparser = Nine30ConcertsParser.new data
+  @concerts = concertsparser.concerts
+
   erb :shows
 end
