d2c2c3490b36700e04b916f78a0cbb78d8c8e793
Romoku <team.romoku@gmail.com>
2012-10-09 04:30:11 -0400

    Remove swap file

---
+++ tests/nine30concertsparser_test.rb
@@ -0,0 +1,42 @@
+require 'test/unit'
+require_relative '../nine30concertsparser.rb'
+
+class TestNine30ConcertsParser < Test::Unit::TestCase
+  @@root = File.expand_path(File.dirname(__FILE__)).freeze
+  @@filename = "930concerts.html".freeze
+
+  def setup
+    html = IO.read(File.join(@@root, @@filename))  
+    parsedhtml = Nokogiri::HTML(html)
+    concertsparser = Nine30ConcertsParser.new parsedhtml
+    @concerts = concertsparser.concerts
+  end
+  
+  def test_concerts_not_null
+    assert_not_nil @concerts
+  end
+
+  def test_concert_date
+    assert_not_nil @concerts[0][:date]
+  end
+
+  def test_concert_title
+    assert_not_nil @concerts[0][:title]
+  end
+
+  def test_concert_time
+    assert_not_nil @concerts[0][:time]
+  end
+
+  def test_concert_price
+    assert_not_nil @concerts[0][:price]
+  end
+  
+  def test_concert_sold_out
+    assert_not_nil @concerts[1][:soldout]
+  end
+
+  def test_concert_buy
+    assert_not_nil @concerts[0][:buy]
+  end
+end
